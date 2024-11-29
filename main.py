ee7711cef42cb4e1da1f1457db7128acfdddc562
jonathansanchez2948 <jonathansanchez2948@correo.itm.edu.co>
2024-11-29 18:06:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def main():
     print("Hola, Profe Soy Fabian Hincapie Castañeda MUCHAS MUCHAS GRACIAS por comportirnos tus conocimientos")
+    print("Excelente trabajo)
  
 if __name__ == "__main__":
     main()
